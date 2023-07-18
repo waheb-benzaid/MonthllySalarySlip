8225e6a482efd1639b4eaeb9cb096171b8873d64
waheb-benzaid <waheb.benzaid@gmail.com>
2023-07-18 19:40:04 +0100

    fix bugs

--- MonthlySalary/Employee.cs
+++ MonthlySalary/Employee.cs
@@ -1,12 +1,24 @@
 ﻿class Employee
 {
+    public const int MinimumLoggedHours = 176;
+    public const decimal OverTimeRate = 1.25m;
     public int Id { get; set; }
-    public string Name { get; set; }
+    public string? Name { get; set; }
     public int LoggedHours { get; set; }
     public int Wage { get; set; }
 
     public virtual decimal CalculateBaseSalary()
     {
-        return LoggedHours * Wage;
+        return LoggedHours * Wage + CalculateAdditionalHoursWage();
+    }
+
+    public decimal CalculateAdditionalHoursWage()
+    {
+        return (LoggedHours - MinimumLoggedHours) * Wage;
+    }
+
+    public override string ToString()
+    {
+        return base.ToString();
     }
 }
--- MonthlySalary/Program.cs
+++ MonthlySalary/Program.cs
@@ -2,6 +2,6 @@
 {
     static void Main(string[] args)
     {
-
+        Employee employee = new Employee();
     }
 }
